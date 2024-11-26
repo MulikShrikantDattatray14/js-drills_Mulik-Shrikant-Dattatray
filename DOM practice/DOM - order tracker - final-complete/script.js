8b5331e217f02549203b2fbead9c0a5adffe77fb
Mulik Shrikant Dattatray <mulikshrisoftware1997@gmail.com>
2024-11-26 07:45:30 +0530

    code refactored for DOM

--- DOM practice/DOM - order tracker - final-complete/script.js
+++ DOM practice/DOM - order tracker - final-complete/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   //DOMContentLoaded is an event in JavaScript that triggers when the HTML document has been completely loaded and parsed( browser reads and understands the HTML code to create a structure it can display and work with.), without waiting for stylesheets, images, and subframes to finish loading.
   let counter = 1;
 
-  const counterElement = document.getElementById("counter");
+  // const counterElement = document.getElementById("counter");
   const incrementButton = document.getElementById("increment");
   const decrementButton = document.getElementById("decrement");
   const circel1Active = document.getElementById("circle1");
@@ -18,49 +18,46 @@ document.addEventListener("DOMContentLoaded", function () {
   circel3Active.classList.remove("active");
   circel4Active.classList.remove("active");
   result.textContent = "Add contact details for further communications";
-  let circle1done = false;
-  let circle2done = false;
-  let circle3done = false;
-  let circel4done = false;
+  // let circle1done = false;
+  // let circle2done = false;
+  // let circle3done = false;
+  // let circel4done = false;
   decrementButton.disabled = true;
 
- 
   decrementButton.style.filter = "blur(0px)";
-  decrementButton.style.cursor="not-allowed"
+  decrementButton.style.cursor = "not-allowed";
   // Increment counter
   incrementButton.addEventListener("click", function () {
     if (counter >= 1 && counter <= 5) {
       counter++;
-      
-      console.log(counter);
+
+      // console.log(counter);
     }
     if (counter == 1) {
-      circle1done = true;
-      circle2done = false;
-      circle3done = false;
-      circel4done = false;
+      // circle1done = true;
+      // circle2done = false;
+      // circle3done = false;
+      // circel4done = false;
 
       circel1Active.classList.add("active");
       circel2Active.classList.remove("active");
       circel3Active.classList.remove("active");
       circel4Active.classList.remove("active");
       result.textContent = "Add contact details for further communications";
-      
+
       decrementButton.textContent = "Previous";
       incrementButton.textContent = "Next";
-     
+
       decrementButton.disabled = true;
 
-    
       decrementButton.style.filter = "blur(0px)";
-       decrementButton.style.cursor="not-allowed"
+      decrementButton.style.cursor = "not-allowed";
     } else if (counter == 2) {
-      circle1done = true;
-      circle2done = true;
-      circle3done = false;
-      circel4done = false;
+      // circle1done = true;
+      // circle2done = true;
+      // circle3done = false;
+      // circel4done = false;
 
-      
       let imgElement = document.createElement("img");
 
       imgElement.src = "checked.png";
@@ -69,8 +66,8 @@ document.addEventListener("DOMContentLoaded", function () {
       imgElement.style.width = "50px";
       imgElement.style.height = "50px";
 
-      circel1Active.innerHTML = ""; 
-      circel1Active.appendChild(imgElement); 
+      circel1Active.innerHTML = "";
+      circel1Active.appendChild(imgElement);
 
       circel2Active.classList.add("active");
       circel1Active.classList.remove("active");
@@ -81,24 +78,21 @@ document.addEventListener("DOMContentLoaded", function () {
       pipe1active.classList.add("done");
       circel1Active.classList.add("done");
       result.textContent = "Add shipping address for successful Delivery";
-    
+
       decrementButton.textContent = "Previous";
 
-     
       decrementButton.disabled = false;
 
-     
       decrementButton.style.filter = "blur(0px)";
       decrementButton.style.cursor = "pointer";
 
       incrementButton.textContent = "Next";
     } else if (counter == 3) {
-      circle1done = true;
-      circle2done = true;
-      circle3done = true;
-      circel4done = false;
+      // circle1done = true;
+      // circle2done = true;
+      // circle3done = true;
+      // circel4done = false;
 
-    
       let imgElement = document.createElement("img");
 
       imgElement.src = "checked.png";
@@ -107,7 +101,7 @@ document.addEventListener("DOMContentLoaded", function () {
       imgElement.style.width = "50px";
       imgElement.style.height = "50px";
       circel2Active.innerHTML = "";
-      circel2Active.appendChild(imgElement);
+      circel2Active.appendChild(imgElement);//adds a specified child node to the end of the list of children of a specified parent node.
 
       circel3Active.classList.add("active");
       circel2Active.classList.remove("active");
@@ -120,23 +114,20 @@ document.addEventListener("DOMContentLoaded", function () {
       pipe2active.classList.add("done");
       circel2Active.classList.add("done");
       result.textContent = "Complete payment to complete the Order";
-    
+
       decrementButton.textContent = "Previous";
 
-      
       decrementButton.disabled = false;
 
-      
       decrementButton.style.filter = "blur(0px)";
       decrementButton.style.cursor = "pointer";
       incrementButton.textContent = "Next";
     } else if (counter == 4) {
-      circle1done = true;
-      circle2done = true;
-      circle3done = true;
-      circel4done = true;
+      // circle1done = true;
+      // circle2done = true;
+      // circle3done = true;
+      // circel4done = true;
 
-     
       let imgElement = document.createElement("img");
 
       imgElement.src = "checked.png";
@@ -145,7 +136,7 @@ document.addEventListener("DOMContentLoaded", function () {
       imgElement.style.width = "50px";
       imgElement.style.height = "50px";
 
-      circel3Active.innerHTML = ""; 
+      circel3Active.innerHTML = "";
       circel3Active.appendChild(imgElement);
 
       circel4Active.classList.add("active");
@@ -160,26 +151,22 @@ document.addEventListener("DOMContentLoaded", function () {
       pipe3active.classList.add("done");
       circel3Active.classList.add("done");
 
-      
       result.textContent = "Ready to get Delivered !";
       incrementButton.textContent = "Finish";
-     
+
       decrementButton.textContent = "Previous";
 
-     
       decrementButton.disabled = false;
 
-      
       decrementButton.style.filter = "blur(0px)";
-      
+
       decrementButton.style.cursor = "pointer";
     } else if (counter == 5) {
-      circle1done = true;
-      circle2done = true;
-      circle3done = true;
-      circel4done = true;
+      // circle1done = true;
+      // circle2done = true;
+      // circle3done = true;
+      // circel4done = true;
 
-     
       let imgElement = document.createElement("img");
 
       imgElement.src = "checked.png";
@@ -187,8 +174,8 @@ document.addEventListener("DOMContentLoaded", function () {
       imgElement.alt = "Circle Image";
       imgElement.style.width = "50px";
       imgElement.style.height = "50px";
-      circel4Active.innerHTML = ""; 
-      circel4Active.appendChild(imgElement); 
+      circel4Active.innerHTML = "";
+      circel4Active.appendChild(imgElement);
       circel4Active.classList.add("active");
       circel3Active.classList.remove("active");
       circel2Active.classList.remove("active");
@@ -202,17 +189,14 @@ document.addEventListener("DOMContentLoaded", function () {
       circel3Active.classList.add("done");
       circel4Active.classList.add("done");
 
-    
       result.textContent = "Order Delivered Succesfully🎉🎉🎉";
 
-     
       decrementButton.remove();
       incrementButton.textContent = "Refresh";
-      
+
       decrementButton.style.cursor = "pointer";
     } else if (counter == 6) {
-     
-      window.location.reload(true);
+      window.location.reload(true);//reloads the current page,
     }
   });
 
@@ -220,13 +204,12 @@ document.addEventListener("DOMContentLoaded", function () {
   decrementButton.addEventListener("click", function () {
     if (counter >= 2 && counter <= 5) {
       counter--;
-      
-    } 
+    }
     if (counter == 1) {
-      circle1done = true;
-      circle2done = false;
-      circle3done = false;
-      circel4done = false;
+      // circle1done = true;
+      // circle2done = false;
+      // circle3done = false;
+      // circel4done = false;
       circel1Active.classList.add("active");
       circel2Active.classList.remove("active");
       circel3Active.classList.remove("active");
@@ -239,20 +222,20 @@ document.addEventListener("DOMContentLoaded", function () {
       divElement.textContent = 1;
 
       circel1Active.innerHTML = "";
-      circel1Active.appendChild(divElement); 
+      circel1Active.appendChild(divElement);
       result.textContent = "Add contact details for further communications";
-      
+
       decrementButton.textContent = "Previous";
 
-      decrementButton.disabled = false;
+      decrementButton.disabled = true;
       decrementButton.style.filter = "blur(0px)";
       incrementButton.textContent = "Next";
-     decrementButton.style.cursor="not-allowed"
+      decrementButton.style.cursor = "not-allowed";
     } else if (counter == 2) {
-      circle1done = false;
-      circle2done = true;
-      circle3done = false;
-      circel4done = false;
+      // circle1done = false;
+      // circle2done = true;
+      // circle3done = false;
+      // circel4done = false;
       circel2Active.classList.add("active");
       circel1Active.classList.remove("active");
 
@@ -266,9 +249,9 @@ document.addEventListener("DOMContentLoaded", function () {
       divElement.textContent = 2;
 
       circel2Active.innerHTML = "";
-      circel2Active.appendChild(divElement); 
+      circel2Active.appendChild(divElement);
       result.textContent = "Add shipping address for successful Delivery";
-     
+
       decrementButton.textContent = "Previous";
       decrementButton.disabled = false;
 
@@ -276,10 +259,10 @@ document.addEventListener("DOMContentLoaded", function () {
       incrementButton.textContent = "Next";
       decrementButton.style.cursor = "pointer";
     } else if (counter == 3) {
-      circle1done = false;
-      circle2done = false;
-      circle3done = true;
-      circel4done = false;
+      // circle1done = false;
+      // circle2done = false;
+      // circle3done = true;
+      // circel4done = false;
       circel3Active.classList.add("active");
       circel2Active.classList.remove("active");
       circel1Active.classList.remove("active");
@@ -288,21 +271,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
       pipe3active.classList.remove("done");
       circel3Active.classList.remove("done");
-     
+
       let divElement = document.createElement("div");
 
       divElement.textContent = 3;
 
       circel3Active.innerHTML = "";
-      circel3Active.appendChild(divElement); 
+      circel3Active.appendChild(divElement);
       result.textContent = "Complete payment to complete the Order";
-     
+
       decrementButton.textContent = "Previous";
 
-     
       decrementButton.disabled = false;
 
-     
       decrementButton.style.filter = "blur(0px)";
       incrementButton.textContent = "Next";
       decrementButton.style.cursor = "pointer";
